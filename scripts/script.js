fa7de3546a34c1f96be621ba52caf460fa0b0051
rtnm120 <37772451+rtnm120@users.noreply.github.com>
2024-03-25 20:26:26 +1100

    Add initial operate() function

--- scripts/script.js
+++ scripts/script.js
@@ -13,3 +13,24 @@ function multiply(x, y) {
 function divide(x, y) {
   return x / y;
 }
+
+function operate(x, y, operator) {
+  let result;
+
+  switch (operator) {
+    case "+":
+      result = add(x, y);
+      break;
+    case "-":
+      result = subtract(x, y);
+      break;
+    case "*":
+      result = multiply(x, y);
+      break;
+    case "/":
+      result = divide(x, y);
+      break;
+  }
+
+  return result;
+}
