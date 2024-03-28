e806d3f36274ca67f7b0ee6caab27f5c9a3c4b7a
rtnm120 <37772451+rtnm120@users.noreply.github.com>
2024-03-28 13:40:45 +1100

    Assign number buttons to numberInput()

--- scripts/script.js
+++ scripts/script.js
@@ -34,3 +34,23 @@ function operate(x, y, operator) {
 
   return result;
 }
+
+function assignNumberButton(btn) {
+  btn.addEventListener("click", (b) => numberInput(b.target));
+}
+
+const numberButtons = [
+  document.querySelector("#btn-nine"),
+  document.querySelector("#btn-eight"),
+  document.querySelector("#btn-seven"),
+  document.querySelector("#btn-six"),
+  document.querySelector("#btn-five"),
+  document.querySelector("#btn-four"),
+  document.querySelector("#btn-three"),
+  document.querySelector("#btn-two"),
+  document.querySelector("#btn-one"),
+  document.querySelector("#btn-zero"),
+  document.querySelector("#btn-decimal"),
+];
+
+numberButtons.forEach(assignNumberButton);
